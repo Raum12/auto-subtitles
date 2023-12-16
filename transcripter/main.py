d3eab7802facddaa5cde95c7ebde9226348976a7
Raum12 <shibani.raum@gmail.com>
2023-12-16 23:15:50 +0100

    Implemented subtitle burner

--- transcripter/main.py
+++ transcripter/main.py
@@ -1,33 +1,47 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
-import moviepy.editor as mp
+from moviepy.editor import VideoFileClip, CompositeVideoClip
 from pytube import YouTube
-import os
-
-# the video id 
-video_id = "rc0mws9NT-0"
-formatter = SRTFormatter()
-
-transcript = YouTubeTranscriptApi.get_transcript(video_id)
-srt_formatted = formatter.format_transcript(transcript)
-print(srt_formatted)
-
+import pysrt, burn_subtitles
 
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
-        youtubeObject.download()
+        youtubeObject.download(filename="no_subtitles.mp4")
     except:
         print("An error has occurred")
     print("Download is completed successfully")
 
-Download("https://www.youtube.com/watch?v=rc0mws9NT-0")
+
+def main():
+    video_id = "rc0mws9NT-0"
+    formatter = SRTFormatter()
+
+    Download(f"https://www.youtube.com/watch?v={video_id}")
+
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    srt_formatted = formatter.format_transcript(transcript)
+
+    with open("subtitles.srt", "w") as file:
+        file.write(srt_formatted)
+
+    mp4_filename = "no_subtitles.mp4"
+    srt_filename = "subtitles.srt"
+
+    video = VideoFileClip(mp4_filename)
+    subtitles = pysrt.open(srt_filename)
+
+    begin, end = mp4_filename.split("mp4")
+    output_video_file = f"{begin}_subtitled.mp4"
+
+    print(f"Output file name: {output_video_file}")
+
+    subtitle_clips = burn_subtitles.create_subtitle_clips(subtitles, video.size)
+    final_video = CompositeVideoClip([video] + subtitle_clips)
+
+    final_video.write_videofile(output_video_file)
 
 
-video = mp.VideoFileClip("video.mp4")
-subtitles = mp.SubtitlesClip("subtitles.srt", video.size) 
-
-final_video = mp.CompositeVideoClip([video, subtitles])
-final_video.write_videofile("output.mp4")
-
+if __name__ == "__main__":
+    main()
