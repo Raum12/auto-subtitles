0362d73c432f269de9d48e53c5392093082541f6
segmentation_fault <shibani.raum@gmail.com>
2023-12-17 16:47:18 +0100

    Allow youtube url input

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
 from moviepy.editor import VideoFileClip, CompositeVideoClip
 from pytube import YouTube
-import pysrt, burn_subtitles, os
+import pysrt, burn_subtitles, os, urllib.parse, requests
 
 def Download(link):
     youtubeObject = YouTube(link)
@@ -12,25 +12,29 @@ def Download(link):
     except:
         print("An error has occurred")
     print("Successfully downloaded video")
-
-
+    
 def main():
     output_dir = "output"
     path = os.path.join(os.getcwd(), output_dir)
     if not os.path.exists(path):
         os.mkdir(path)
 
-    video_id = input("Enter the video ID: ")
-    language = input("Enter language to translate to (e.g. 'ar', 'de'): ")
-    formatter = SRTFormatter()
+    url_data = urllib.parse.urlparse(input("Enter the video url: "))
+    query = urllib.parse.parse_qs(url_data.query)
+    video_id = query["v"][0]
 
+    input_language = input("Select the language of the video (e.g. 'en', 'ar'): ")
+    output_language = input("Enter language to translate to (e.g. 'en', 'ar'): ")
+
+    formatter = SRTFormatter()
     Download(f"https://www.youtube.com/watch?v={video_id}")
 
-    # transcript = YouTubeTranscriptApi.get_transcript(video_id)
     transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-    transcript = transcript_list.find_transcript(['en'])
+    print(transcript_list)
+    transcript = transcript_list.find_transcript([input_language])
 
-    translated_transcript = transcript.translate(language)
+    return 0 
+    translated_transcript = transcript.translate(output_language)
     srt_formatted = formatter.format_transcript(translated_transcript.fetch())
 
     print("Writing SRT file...")
@@ -47,7 +51,7 @@ def main():
     output_video_file = "output/subtitled.mp4"
 
     print("Burning subtitles into video...")
-    subtitle_clips = burn_subtitles.create_subtitle_clips(subtitles=subtitles, videosize=video.size, language=language)
+    subtitle_clips = burn_subtitles.create_subtitle_clips(subtitles=subtitles, videosize=video.size, language=output_language)
     print("Successfully burned subtitles into video")
 
     final_video = CompositeVideoClip([video] + subtitle_clips)
