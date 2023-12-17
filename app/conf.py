81c61ed4be51cd7c47683520b14e9e1f4f7a07b7
Raum12 <shibani.raum@gmail.com>
2023-12-17 13:33:34 +0100

    Edit binary file detection for posix

--- app/conf.py
+++ app/conf.py
@@ -3,8 +3,8 @@ import os, platform
 if platform.system() == "Windows":
     IMAGEMAGICK_BINARY = os.getenv("IMAGEMAGICK_BINARY", "C:\Program Files\ImageMagick-7.0.8-Q16\convert.exe")
 
-elif platform.system() == "Darwin":
-    IMAGEMAGICK_BINARY = os.getenv("IMAGEMAGICK_BINARY", "/usr/local/bin/magick") 
+# elif platform.system() == "Darwin":
+#     IMAGEMAGICK_BINARY = os.getenv("IMAGEMAGICK_BINARY", "/usr/local/bin/magick") 
 
-elif platform.system() == "Linux":
-    IMAGEMAGICK_BINARY = os.environ.get("IMAGEMAGICK_BINARY", "/usr/bin/convert")
+# elif platform.system() == "Linux":
+#     IMAGEMAGICK_BINARY = os.environ.get("IMAGEMAGICK_BINARY", "/usr/bin/convert")
