8c96c785f7861f4dbb7b990b5896bbd3c9507b0c
segmentation_fault <shibani.raum@gmail.com>
2023-12-22 20:49:52 +0100

    Finished

--- app/app.py
+++ app/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, redirect, url_for, send_file, send_from_directory
 from werkzeug.utils import secure_filename
 from subtitler import generate_translated_subs
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "upload_folder"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
@@ -10,9 +13,14 @@ def index():
 
 @app.route('/read-form', methods=['POST'])
 def read_form():
-   data = request.form
+    data = request.form
+    video_url = data["video_url"]
    
-   return data["video_url"]
+    generate_translated_subs(video_url)
+   
+    # return send_file("video.mp4", as_attachment=True)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], "video.mp4", as_attachment=True)
+    # return redirect(url_for('download_file', name="video.mp4"))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/subtitler.py
+++ app/subtitler.py
@@ -2,7 +2,7 @@ from pytube import YouTube
 import os, urllib.parse, requests
 from flask import send_file
 
-video_path = "video.mp4"
+video_path = "upload_folder/video.mp4"
 
 def Download(link):
     youtubeObject = YouTube(link)
@@ -13,16 +13,17 @@ def Download(link):
         print("An error has occurred")
     print("Successfully downloaded video")
     
-def generate_translated_subs():
+def generate_translated_subs(video_url):
     output_dir = "output"
     path = os.path.join(os.getcwd(), output_dir)
     if not os.path.exists(path):
         os.mkdir(path)
 
-    url_data = urllib.parse.urlparse(input("Enter the video url: "))
+    url_data = urllib.parse.urlparse(video_url)
     query = urllib.parse.parse_qs(url_data.query)
     video_id = query["v"][0]
 
     Download(f"https://www.youtube.com/watch?v={video_id}")
     
     os.system(f"auto_subtitle {video_path} --task translate")
+    os.rename("video.mp4", "upload_folder/video.mp4")
