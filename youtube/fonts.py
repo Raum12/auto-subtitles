b2eae2dc9ed4924b144defcd343932cc8de47db2
raumshiba <shibani.raum@gmail.com>
2023-12-22 18:45:52 +0100

    Delete fonts.py

--- youtube/fonts.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import TextClip
-
-fonts = TextClip.list("font")
-
-for i, font in enumerate(fonts): print(f"{i}. {font}\n")
-# if "Noto-Sans-Arabic" in fonts:
-#     print("Match")
-# else:
-#     print("No match")
