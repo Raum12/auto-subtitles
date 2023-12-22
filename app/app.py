29e5720114e7f46808c4abd1f2f5698f0fe4bdc3
raumshiba <shibani.raum@gmail.com>
2023-12-22 23:35:20 +0100

    Delete video file after local download

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file, send_from_directory
-from werkzeug.utils import secure_filename
+from flask import Flask, after_this_request, render_template, request, send_from_directory
 from subtitler import generate_translated_subs
+import os
 
 app = Flask(__name__)
 
@@ -15,8 +15,20 @@ def index():
 def read_form():
     data = request.form
     video_url = data["video_url"]
+    
+    file_path = "upload_folder/video.mp4"
    
     generate_translated_subs(video_url)
+    file_handle = open(file_path, 'r')
+    
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(file_path)
+            file_handle.close()
+        except Exception as error:
+            app.logger.error("Error removing or closing downloaded file handle", error)
+        return response
    
     # return send_file("video.mp4", as_attachment=True)
     return send_from_directory(app.config['UPLOAD_FOLDER'], "video.mp4", as_attachment=True)
